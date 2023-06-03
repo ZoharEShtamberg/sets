28c43355a068aff28117d0db884f8d40e5715ae2
Zohar <a@b>
2023-06-03 17:29:55 +0300

    mend

--- test.cpp
+++ test.cpp
@@ -3,25 +3,26 @@
 using std::cout;
 using std::endl;
 int main() {
-Set set1, set2;
-for (int j = 0; j < 20; j += 2) {
-set1.add(j);
+Set<char> set1;
+Set<char> set2;
+for (int j = 6; j < 20; j += 2) {
+set1.add((char)j);
 }
-for (int j = 0; j < 12; j += 3) {
-set2.add(j);
+for (int j = 6; j < 12; j += 3) {
+set2.add((char)j);
 }
-Set unionSet = unionSets(set1,set2);
-Set intersectionSet = intersection(set1,set2);
+Set<char> unionSet = unionSets(set1,set2);
+Set<char> intersectionSet = intersection(set1,set2);
 cout << set1.toString() << endl;
 cout << set2.toString() << endl;
 cout << unionSet.toString() << endl;
 cout << intersectionSet.toString() << endl;
 cout << "now using iterators" << endl;
-for(Set::Iterator it = set1.begin(); it != set1.end(); ++it) {
+for(Set<char>::Iterator it = set1.begin(); it != set1.end(); ++it) {
 cout << *it << endl;
 }
 cout << "now using range-based for loop" << endl;
-for(int x: set2) {
+for(char x: set2) {
 	cout << x << endl;
 }
 return 0;
